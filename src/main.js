05ffb7747bf4424f509d4bea966f5542c85856db
adibkn1 <55048595+adibkn1@users.noreply.github.com>
2024-02-05 19:28:57 +0530

    c

--- src/main.js
+++ src/main.js
@@ -45,8 +45,10 @@ import {
     // Set the source of the CameraKit session.
     await session.setSource(source);
   
+   
+   
     // Set the render size of the CameraKit session to the size of the browser window.
-    session.source.setRenderSize( window.innerWidth,  window.innerHeight);
+    session.source.setRenderSize( window.innerWidth,  window.innerHeight); 
   
     // Start the CameraKit session.
     session.play(); 
